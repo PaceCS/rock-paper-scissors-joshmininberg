b01b07980dd981902bc7414a821056c6b9105ea1
Programming <programming@us-physics-cart-07.local>
2018-04-18 13:03:31 -0400

    finished rps

--- logic.js
+++ logic.js
@@ -1,12 +1,11 @@
-
 // This array will hold the options for the game
 // Fill in your options below
-const options = ["rock", "paper","scissors"];
+const options = ['rock', 'paper', 'scissors'];
 
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
     // First remind the user of his/her choice
-    console.log("You chose"+ userchoice);
+    console.log('You chose ' + userChoice);
 
     // Now choose a random number between 0 and 2
     const rand = Math.floor(Math.random() * 3);
@@ -15,35 +14,27 @@ function playGame(userChoice) {
     const compChoice = options[rand];
 
     // Now log the computer's choice to the console.
-    console.log(The computer chose);
-if(userchoice == compChoice){
-    console.log("You tied")
-}else{
-    if(userChoice == "rock"){
-        if(compChoice == "paper"){
-            console.log("You lose.");
-        }else{
-            console.log("You win.");
+    console.log('The computer chose ' + compChoice);
+    if (userChoice == compChoice) {
+        console.log('You tied');
+    } else if (userChoice == 'rock') {
+        if (compChoice == 'paper') {
+            console.log('You lose.');
+        } else {
+            console.log('You win.');
         }
-    } else if (userChoice == "paper"){
-            if(compChoice == "rock") {
-                console.log("You win.");
-        }else{
-            console.log("You lose.");
+    } else if (userChoice == 'paper') {
+        if (compChoice == 'rock') {
+            console.log('You win.');
+        } else {
+            console.log('You lose.');
         }
+    } else if (compChoice == 'paper') {
+        console.log('You win.');
     } else {
-        if(compChoice == "paper"){
-            console.log("You win.");
-    }else {
-        console.log("You lose");
+        console.log('You lose');
     }
-
-
-
-
-
-
-
+}
 
 module.exports.playGame = playGame;
 module.exports.options = options;
